040ce0fb9afb2bb4398647478e8628f9913676d1
Le Dinh Quyen <quyenld9699@gmail.com>
2025-05-07 11:46:08 +0700

    update init function

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 import fs from "fs-extra";
 import path from "path";
 import chalk from "chalk";
-import findUp from "find-up";
+import { findUpSync } from "find-up";
 
 interface CliConfig {
     targetDirectory: string;
@@ -17,7 +17,7 @@ const CONFIG_FILE_NAME = "crypto-mui-icon-cli.json";
 function findProjectRoot(): string {
     try {
         // Look for package.json up the directory tree
-        const packageJsonPath = findUp.sync("package.json");
+        const packageJsonPath = findUpSync("package.json");
         if (packageJsonPath) {
             return path.dirname(packageJsonPath);
         }
